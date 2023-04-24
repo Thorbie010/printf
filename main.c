26df268093eafa206f416f10ce639af4612eebe0
thorbie <jonahemmanuel54@gmail.com>
2023-04-24 12:42:01 -0700

    edit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+int main() 
+{
+    char my_char = 'a';
+    int my_int = 123;
+    char my_string[] = "Hello, world!";
+
+    _printf("Character: %c\n", my_char);
+    _printf("Integer: %d\n", my_int);
+    _printf("String: %s\n", my_string);
+
+    printf("Character: %c\n", my_char);
+    printf("Integer: %d\n", my_int);
+    printf("String: %s\n", my_string);
+    
+    return (0);
+}
