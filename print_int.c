778a9d2051bb659b1b355d9d4a4b1bd49b8c7d36
thorbie <jonahemmanuel54@gmail.com>
2023-04-24 11:35:24 -0700

    edit to print int

--- print_int.c
+++ print_int.c
@@ -1,35 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 
 /**
  * print_int - function pointer for printing integer
  * @args: variable argument integer input
- * Return: len
+ * Return: value for len
+ *
  */
 
 int print_int(va_list args)
 {
-	long int n = va_arg(args, long int);
+	int64_t n = va_arg(args, int64_t);
 	int len = 0;
+	int digit;
 
-	if (n < 0)
+	if (n == 0)
+	{
+		_putchar('0');
+		len++;
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 		len++;
 	}
-	if (n > 9)
+	while (n > 0)
 	{
-		va_list args_copy;
-
-		va_copy(args_copy, args);
-		len += print_int(args);
-		va_end(args_copy);
-
+		digit = n % 10;
+		_putchar(digit + '0');
+		n /= 10;
+		len++;
 	}
-	_putchar(n % 10 + '0');
-	len++;
-
 	return (len);
 }
+
