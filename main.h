b2e641458d2414df15a780841f73da5f4e5d642f
Endurance Aneke <125446212+theenduranceA@users.noreply.github.com>
2023-04-25 23:22:03 +0100

    Update main.h

--- main.h
+++ main.h
@@ -36,24 +36,25 @@ typedef struct printSpecifier
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
-int _print_char(va_list args, flags_t *f);
-int _print_string(va_list args, flags_t *f);
-int _print_percent(va_list args, flags_t *f);
-int _print_int(va_list args, flags_t *f);
-int _print_int(va_list args, flags_t *f);
-int _print_binary(va_list args, flags_t *f);
-int _print_unsigned(va_list args, flags_t *f);
-int _print_octal(va_list args, flags_t *f);
-int _print_hex(va_list args, flags_t *f);
-int _print_HEX(va_list args, flags_t *f);
-int _print_STRING(va_list args, flags_t *f);
-int _print_pointer(va_list args, flags_t *f);
-int _print_reverse(va_list args, flags_t *f);
-int _print_rot13(va_list args, flags_t *f);
+int _print_char(va_list args);
+int _print_string(va_list args);
+int _print_percent(va_list args);
+int _print_int(va_list args);
+int _print_int(va_list args);
+int _print_binary(va_list args);
+int _print_unsigned(va_list args);
+int _print_octal(va_list args);
+int _print_hex(va_list args);
+int _print_HEX(va_list args);
+int _print_STRING(va_list args);
+int _print_pointer(va_list args);
+int _print_reverse(va_list args);
+int _print_rot13(va_list args);
+int (*get_func(char c))(va_list)
 int count_digit(int i);
 void _print_number(int n);
 char *convert(unsigned int num, int base, int lowercase);
-int (*get_print(char s))(va_list, flags_t *);
-int get_flag(char s, flags_t *f);
+int (*get_print(char s))(va_list);
+int get_flag(char s);
 
 #endif
