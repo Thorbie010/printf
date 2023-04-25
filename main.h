3da6deae987a3fa4cfa44eddc346de762ffc0006
theenduranceA <theenduranceaneke@gmail.com>
2023-04-25 15:39:13 -0700

    main.h

--- main.h
+++ main.h
@@ -8,20 +8,6 @@
 #include <stdarg.h>
 
 /**
- * struct flags - Function to print flags
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @pound: flag for the '#' character
- */
-
-typedef struct flags
-{
-	int plus;
-	int space;
-	int pound;
-} flags_t;
-
-/**
  * struct printSpecifier - Function to print specifiers.
  * @z: The operators to be printed.
  * @f: The function
@@ -30,30 +16,30 @@ typedef struct flags
 typedef struct printSpecifier
 {
 	char z;
-	int (*f)(va_list args, flags_t *f);
+	int (*f)(va_list args);
 } ps;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
-int _print_char(va_list args, flags_t *f);
-int _print_string(va_list args, flags_t *f);
-int _print_percent(va_list args, flags_t *f);
-int _print_int(va_list args, flags_t *f);
-int _print_int(va_list args, flags_t *f);
-int _print_binary(va_list args, flags_t *f);
-int _print_unsigned(va_list args, flags_t *f);
-int _print_octal(va_list args, flags_t *f);
-int _print_hex(va_list args, flags_t *f);
-int _print_HEX(va_list args, flags_t *f);
-int _print_STRING(va_list args, flags_t *f);
-int _print_pointer(va_list args, flags_t *f);
-int _print_reverse(va_list args, flags_t *f);
-int _print_rot13(va_list args, flags_t *f);
+int _print_char(va_list args);
+int _print_string(va_list args);
+int _print_percent(va_list args);
+int _print_int(va_list args);
+int _print_int(va_list args);
+int _print_binary(va_list args);
+int _print_unsigned(va_list args);
+int _print_octal(va_list args);
+int _print_hex(va_list args);
+int _print_HEX(va_list args);
+int _print_STRING(va_list args);
+int _print_pointer(va_list args);
+int _print_reverse(va_list args);
+int _print_rot13(va_list args);
 int count_digit(int i);
 void _print_number(int n);
 char *convert(unsigned int num, int base, int lowercase);
 int (*get_print(char s))(va_list, flags_t *);
-int get_flag(char s, flags_t *f);
+int get_flag(char s);
 
 #endif
