d0c869912d062085ba7d3ff70210ab7c629503e5
Jonah <jonahemmanuel54@gmail.com>
2023-04-26 00:19:14 -0700

    fix

--- helper_function.c
+++ helper_function.c
@@ -69,7 +69,8 @@ int print_int(va_list args)
 		temp /= 10;
 	}
 	digit = (int *)malloc(len * sizeof(int));
-
+	if (digit == NULL)
+		return (-1);
 	if (n == 0)
 	{
 		_putchar('0');
@@ -104,33 +105,21 @@ int print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
 	int count = 0;
-	char *temp_str;
 
 	if (str == NULL || str == (char *)0)
 	{
 		str = "(null)";
 	}
-	else 
-	{
-		temp_str = (char *)malloc(strlen(str) + 1);
-		if (temp_str == NULL)
-			return (-1);
-
-		strcpy(temp_str, str);
-		str = temp_str;
-	}
-
 	while (*str != '\0')
 	{
 		if (_putchar(*str) == EOF)
 		{
-			free(str);
 			return (-1);
 		}
 		str++;
 		count++;
 	}
-	free(str);
+
 	return (count);
 }
 
